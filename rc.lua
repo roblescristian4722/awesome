e388e294223bc28d94fbabcc08c342a3234a108f
roblescristian991 <roblescristian991@gmail.com>
2021-04-08 21:13:33 -0500

    back to gnome-terminal

--- rc.lua
+++ rc.lua
@@ -70,7 +70,7 @@ beautiful.wallpaper = "/mnt/HDD/Wallpapers/blade.runner-1920-874022.jpg"
 beautiful.font = "FontAwesome 9"
 
 -- This is used later as the default terminal and editor to run.
-terminal = "tilix"
+terminal = "gnome-terminal"
 editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
 
